0805098a9ce6bb1aab0c2a725003dbfb211fa25c
ilhamarfian <ilham.arfian@btpn.com>
2025-02-04 16:36:42 +0700

    httptest for testing http handler

---
+++ http_test.go
@@ -0,0 +1,27 @@
+package learn_golang_web
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func TestHelloHandler(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	assert.Equal(t, bodyString, "Hello, World!\n")
+}
