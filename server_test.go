cbc307d1cf98622ca04f96e0985a958e40289a84
ilhamarfian <ilham.arfian@btpn.com>
2025-02-04 16:24:41 +0700

    server mux

--- server_test.go
+++ server_test.go
@@ -31,3 +31,36 @@ func TestHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+// TestServeMux ServeMux will combine all http handler function into single handler (commonly called router)
+// Parameters:
+//
+//	t - The testing object used to manage
+func TestServeMux(t *testing.T) {
+	var mux *http.ServeMux = http.NewServeMux()
+
+	// adding "/" after route name will make route with /* redirected to this handler
+	mux.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "images")
+		if err != nil {
+			return
+		}
+	})
+
+	mux.HandleFunc("/images/thumbnails", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "thumbnails")
+		if err != nil {
+			return
+		}
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux, // mux implements Handler interface which has method ServeHTTP
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
