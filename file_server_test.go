9b97f60c49279e8f0edf3413072ca50b03e8c102
ilhamarfian <ilham.arfian@btpn.com>
2025-02-13 13:29:12 +0700

    file server

---
+++ file_server_test.go
@@ -0,0 +1,49 @@
+package learn_golang_web
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"testing"
+)
+
+func TestFileServer(t *testing.T) {
+	directory := http.Dir("./resources")
+	fileServer := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	// strip prefix to remove /static from URL when accessing files
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+//go:embed resources/*
+var resources embed.FS
+
+func TestFileServerGolangEmbed(t *testing.T) {
+	// Use fs.Sub to create a new file server with a subdirectory of resources.
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS(directory))
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
